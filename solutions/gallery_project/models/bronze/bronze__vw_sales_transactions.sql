b24c04555cee0a78fc3344231c76111928017890
Anya Prosvetova <47951483+anyalitica@users.noreply.github.com>
2025-09-14 21:01:58 +0200

    Fixed typo

--- solutions/gallery_project/models/bronze/bronze__vw_sales_transactions.sql
+++ solutions/gallery_project/models/bronze/bronze__vw_sales_transactions.sql
@@ -4,4 +4,4 @@ from {{ source('gallery_data', 'sales_transactions') }}
 
 -- Deduplication statement
 
-qualify row_number() over (partition dbt by transaction_id order by sale_date desc) = 1
+qualify row_number() over (partition by transaction_id order by sale_date desc) = 1
